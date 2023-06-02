99829651cde49d215adea87dd7db1161d8c2da43
kimjongmin <qorrh78@naver.com>
2023-06-02 16:29:50 +0900

    연습

---
+++ ParkingNetwork/app/src/main/java/kr/ac/mokwon/parkingnetwork/JoinActivity.java
@@ -0,0 +1,18 @@
+package kr.ac.mokwon.parkingnetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+        getSupportActionBar().hide();
+    }
+}
---
+++ ParkingNetwork/app/src/main/java/kr/ac/mokwon/parkingnetwork/LoginActivity.java
@@ -0,0 +1,18 @@
+package kr.ac.mokwon.parkingnetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+    }
+}
